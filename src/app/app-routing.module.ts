3fe8d6705646926486c068703123ee17ca6cebd2
KaranDuggal <duggalkaran400@gmail.com>
2021-05-05 11:22:04 +0530

    open start page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home",
+    loadChildren:()=>import('./components/core/home/home.module').then((mod)=>mod.HomeModule)
+  },
+  {
+    path:"**",
+    redirectTo:"home"
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
